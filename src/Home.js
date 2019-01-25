13a18f9efa4a3535bce35bf01f2a5834f9568bd1
silasonyango93 <silas.onyango93@gmail.com>
2019-01-25 16:24:54 +0300

    More tweaks

--- src/Home.js
+++ src/Home.js
@@ -1,77 +1,60 @@
-import React from 'react';
-import $ from 'jquery'
-import Table from './Table.js';
+import React from "react";
+import $ from "jquery";
+import Table from "./Table.js";
 import axios from "axios";
 
-
-
 class Home extends React.Component {
-	
-	
-	constructor(props){
-        super(props);
-        this.state = {
-            data: 
-         [
-            {
-               "id":1,
-               "name":"Foo",
-               "age":"20"
-            },
-            {
-               "id":2,
-               "name":"Bar",
-               "age":"30"
-            },
-            {
-               "id":3,
-               "name":"Baz",
-               "age":"40"
-            }
-         ],star_wars_characters : []
-        };
-		
-		//this.updateMsg = this.updateMsg.bind(this);
-    }
-	
-	componentDidMount() {
-		
-     fetch('https://swapi.co/api/people/?format=json')
-     .then(function(response) {
-      return response.json();
-     })
-    .then(function(myJson) {
-     console.log(JSON.stringify(myJson.results));
-		 this.setState({
-            star_wars_characters: myJson.results
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {
+          id: 1,
+          name: "Foo",
+          age: "20"
+        },
+        {
+          id: 2,
+          name: "Bar",
+          age: "30"
+        },
+        {
+          id: 3,
+          name: "Baz",
+          age: "40"
+        }
+      ],
+      star_wars_characters: []
+    };
+
+    
+  }
+
+  componentDidMount() {
+    fetch("https://swapi.co/api/people/?format=json")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+      //   console.log(data.results);
+        this.setState({
+          ...this.state,
+          data: data.results
         });
-		 
-		 
-	 });
-		
-		
+      });
+
   }
 	
 	
-	
-	
-   render() {
-      return (
-         <div>
-            
-            
-                  
-		  
-		  <Table dataProp={this.state.data} />
-               
-         </div>
-      );
-   }
-	
-	
+    
+
+  render() {
+    return (
+      <div>
+        <Table dataProp={this.state.data} />
+      </div>
+    );
+  }
 }
-
-
-
 
 export default Home;
--- src/TableRow.js
+++ src/TableRow.js
@@ -1,12 +1,29 @@
 import React from 'react';
 class TableRow extends React.Component {
+	
+  constructor(props) {
+    super(props);
+
+    
+	  
+	  this.open_new_tab = this.open_new_tab.bind(this);
+   }
+	
+	
+   open_new_tab() {
+	   const url = 'https://github.com/silasonyango93/my_react/blob/master/src/Home.js';
+        window.open(url, '_blank');
+	   console.log("Imekubali");
+    }  
+	
    render() {
       return (
 		  
-         <tr>
-            <td>{this.props.data.id}</td>
+         <tr onClick={this.open_new_tab}>
             <td>{this.props.data.name}</td>
-            <td>{this.props.data.age}</td>
+            <td>{this.props.data.height}</td>
+            <td>{this.props.data.mass}</td>
+		    <td>{this.props.data.gender}</td>
          </tr>
 		  
             
